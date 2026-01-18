a72b39d9614451e3683ca3db43fa0e7fdc811dda
Sahil-Mansuri07 <sahilmansuri0021@gmail.com>
2026-01-18 16:23:17 +0530

    Starting with Express.js

---
+++ Lecture_08/index.js
@@ -0,0 +1,19 @@
+// Starting with Express.js..
+
+const express=require("express");
+
+const app=express();
+
+app.get("/",(req, res)=>{
+
+    return res.send("Hello from Home Page");
+
+});
+
+app.get("/about",(req, res)=>{
+
+    return res.send(`Hello ${req.query.name}`);
+
+});
+
+app.listen(8080,()=>console.log("Server started...."));
